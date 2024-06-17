1c150741641e564d8199ea1d374f16a467b75d33
Samuel Paget <sjapaget@gmail.com>
2024-06-17 22:55:34 +0200

    add button count detection

---
+++ button_counter_lcd/app.py
@@ -0,0 +1,24 @@
+from gpiozero import Button
+
+button = Button(17)
+
+count = 0
+
+def onButtonPressed():
+    count += 1
+    print("Button is pressed, count is #{count}")
+
+
+def loop():
+    button.when_pressed = whenButtonPressed
+
+def destroy():
+    button.close()
+
+if __name__ == '__main__':
+    print ('Program is starting ... ') 
+    try:
+        loop()
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        destroy()
+        print("Ending program")
